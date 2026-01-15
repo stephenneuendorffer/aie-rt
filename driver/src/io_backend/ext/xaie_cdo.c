267adb214e89d1683537e8dd47a4893876486caa
Jeff Fifield <jeff.fifield@amd.com>
2026-01-15 14:42:11 -0700

    Add a workaround for cdo_rts.h
    Previously this was handled by patching bootgen, but mlir-aie already
    maintains a branch of aie-rt with similar patch for simulation "main_rts.h",
    and patching here allows bootgen to be used by mlir-aie unpatched.

--- driver/src/io_backend/ext/xaie_cdo.c
+++ driver/src/io_backend/ext/xaie_cdo.c
@@ -29,7 +29,13 @@
 
 #ifdef __AIECDO__ /* AIE simulator */
 
-#include "cdo_rts.h"
+#include "stdint.h"
+
+void cdo_Write32(uint64_t, uint32_t);
+void cdo_MaskWrite32(uint64_t , uint32_t, uint32_t);
+void cdo_MaskPoll(uint64_t , uint32_t, uint32_t, uint32_t);
+void cdo_BlockWrite32(uint64_t, const uint32_t*, uint32_t);
+void cdo_BlockSet32(uint64_t, uint32_t, uint32_t);
 
 #endif
 
